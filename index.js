7e8d30c6bb3d445949bc8a0da1edb859a2b5e984
Mstars_Lab1_13 <mstars_lab1_13@Mstars-Lab1-13s-iMac.local>
2022-02-09 15:38:33 +0800

    finished book APIs

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
+const apiRoutes = require('./routes/api')
+const adminRoutes = require('./routes/admin')
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
+
+app.use('/api', apiRoutes)
+app.use("/", adminRoutes)
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
---
+++ routes/api.js
@@ -0,0 +1,108 @@
+const express = require("express");
+const router = new express.Router();
+const data = require('../book.json'); 
+const app = require("./admin");
+
+//list of books  
+books = data.books;
+
+// 1. send 3 random books
+router.get('/random', (req, res)=>{
+    do {
+        var first = Math.floor(Math.random() * books.length)
+        var second = Math.floor(Math.random() * books.length)
+        var third = Math.floor(Math.random() * books.length)
+    }while (!(first !== second && first!==second && second !== third ))
+    let random = []
+    random.push(books[first])
+    random.push(books[second])
+    random.push(books[third])
+    res.send(random)
+})
+
+//2. sort by publish date
+router.get('/sort/:value', (req, res) =>{
+    let sortBy = req.params.value
+    sorted = books.sort(function(a,b){
+        if (sortBy == "published"){
+            return Date.parse(b.sortBy) - Date.parse(a.sortBy)
+        }else{
+            return b.sortBy - a.sortBy
+        }
+    })
+    res.send(sorted)
+})
+
+// 3. send authors
+router.get('/authors', (req, res)=>{
+    authors = []
+    books.forEach(book => {
+        authors.push(book.author)
+    });
+    res.send(authors.sort())
+})
+
+// 4. send all books
+router.get('/books', (req, res)=>{
+    res.send(books)
+})
+
+// 5. send book by isbn 
+router.get("/book/:isbn", (req, res)=>{
+    isbn = req.params.isbn
+    book = books.filter( book =>{
+        return book.isbn === isbn
+    })
+    res.send(book)
+})
+// 6. send book by title using query parameter
+router.get("/search", (req, res)=>{
+    title = req.query.title
+    result = books.filter(book=>{
+        return book.title.includes(title);
+    })
+    res.send(result)
+})
+
+// 7. send thickest book
+router.get("/thickBook", (req, res)=>{
+    temp = {}
+    max = 0
+    books.forEach(book=>{
+        if(max < book.pages){
+            max = book.pages
+            temp = book
+        }
+    })
+    res.send(temp)
+})
+// 8. send thinnest book
+router.get('/thinBook', (req, res)=>{
+    temp = {}
+    min = 10000;
+    books.forEach(book=>{
+        if(min > book.pages){
+            min = book.pages
+            temp = book
+        }
+    })
+    res.send(temp)
+})
+// 9. list books of publishers
+router.get("/publisherBooks", (req,res)=>{
+    const map = new Map();
+    books.map(book => {
+        let counter = 0;     
+        if(!map.has(book.publisher)) {
+            map.set(book.publisher, 1);
+        } else {
+            counter = map.get(book.publisher);
+            map.set(book.publisher, counter+1);
+            console.log(counter);
+        }
+        return book.publisher
+    })
+    res.send(Object.fromEntries(map))
+})
+module.exports = router;
+
